878639b6ab029c0474867824b8eceb894944d7b7
moatasem <moatasemalnaimat@gmail.com>
2024-08-05 00:37:54 +0300

    almost finish tasks list

--- assets/scripts/home.js
+++ assets/scripts/home.js
@@ -1,41 +1,132 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    class Task {
+        #name;
+        static parentElement
+        constructor(taskName) {
+            this.#name = taskName;
+        }
+
+        setName(taskName) {
+            this.#name = taskName;
+        }
+
+        getName() {
+            return this.#name;
+        }
+
+        addTask() {
+            const tempDiv = document.createElement("div");
+            tempDiv.innerHTML = `<li class="shadow-sm bg-main-white rounded mb-3 d-flex align-items-center task">
+                                    <input id="taskid2" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
+                                    <label role="button" for="taskid2" class="w-100 p-3">
+                                        <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
+                                        <span class="">${this.#name}</span>
+                                    </label>
+                                    <i role="button" class="fa-solid fa-pen-to-square text-main-orange me-3 edit-task-btn"></i>
+                                    <i role="button" class="fa-solid fa-trash text-main-orange me-3 delete-task-btn"></i>
+                                </li>`;
+            let taskElement = tempDiv.firstChild;
+            Task.parentElement.appendChild(taskElement);
+            handelTaskClicked(taskElement);
+            addDeleteListner(taskElement);
+            return taskElement;
+        }
+
+        static deleteTask(taskElement) {
+            Task.parentElement.removeChild(taskElement);;
+        }
+
+        static deleteAllTasks() {
+            let allTasks = document.querySelectorAll(".task");
+            allTasks.forEach(task => {
+                Task.parentElement.removeChild(task);
+            });
+        }
+
+    }
+
+    function handelTaskClicked(taskElement) {
+
+        let taskLabe = taskElement.querySelector("label");
+
+        taskLabe.addEventListener("click", () => {
+
+            let taskIcon = taskElement.querySelector(".task-icon");
+            let taskInput = taskElement.querySelector(".task__input");
+
+            if (taskInput.checked) {
+                taskIcon.classList.remove('fa-solid', 'fa-square-check');
+                taskIcon.classList.add('fa-regular', 'fa-square');
+            } else {
+                taskIcon.classList.remove('fa-regular', 'fa-square');
+                taskIcon.classList.add('fa-solid', 'fa-square-check');
+            }
+            taskElement.classList.toggle('task--checked');
+        });
+    }
+
+    function addDeleteListner(taskElement) {
+        let deleteTaskBtn = taskElement.querySelector(".delete-task-btn");
+        deleteTaskBtn.addEventListener("click", () => {
+            Task.deleteTask(taskElement);
+        });
+    }
+
+    function addEditListner(taskElement, newTaskName) {
+        let editTaskBtn = taskElement.querySelector(".edut-task-btn");
+        editTaskBtn.addEventListener("click", () => {
+            taskElement.innerText = newTaskName;
+        });
+    }
+
     //show username
     let userNameEl = document.getElementById("userName");
     let userName = localStorage.getItem('username');
     userNameEl.innerText = userName + '!';
 
 
-    //control checkbox icon
-    let taskIsnputs = document.querySelectorAll(".task__input");
-
-    taskIsnputs.forEach(taskInput => {
-        taskInput.addEventListener("click", () => {
-
-            let taskInputParent = taskInput.parentElement;
-            let taskIcon = taskInputParent.querySelector(".task-icon");
-
-            if (taskInput.checked) {
-                taskIcon.classList.remove('fa-regular', 'fa-square');
-                taskIcon.classList.add('fa-solid', 'fa-square-check');
-            } else {
-                taskIcon.classList.remove('fa-solid', 'fa-square-check');
-                taskIcon.classList.add('fa-regular', 'fa-square');
-            }
-            taskInputParent.classList.toggle('task--checked');
-        });
+    //handel clicked tasks
+    let tasksElements = document.querySelectorAll(".task");
+    tasksElements.forEach(taskElement => {
+        handelTaskClicked(taskElement);
     });
+
+
+    //set tasks parent elemet
+    let TasksParent = document.getElementById("TasksParent");
+    Task.parentElement = TasksParent;
 
     //delete all tasks
     let DeleteAllTasks = document.getElementById("DeleteAllTasks");
-    let TasksParent = document.getElementById("TasksParent");
-    let allTasks = document.querySelectorAll(".task");
 
     DeleteAllTasks.addEventListener("click", () => {
-        allTasks.forEach(task => {
-            TasksParent.removeChild(task);
-        });
+        Task.deleteAllTasks();
     });
 
+
+    //add new task
+    let addTaskModalElement = document.getElementById("addTaskModal");
+    const addTaskModal = new bootstrap.Modal(addTaskModalElement);
+    let SaveTaskBtn = document.getElementById("SaveTaskBtn");
+    let taskNameElement = document.getElementById("taskName");
+
+    SaveTaskBtn.addEventListener("click", () => {
+        let task = new Task(taskNameElement.value);
+        task.addTask();
+        taskNameElement.value = '';
+        addTaskModal.hide();
+    });
+
+
+    //delete task
+    let tasks = document.querySelectorAll(".task");
+
+    tasks.forEach(task => {
+        addDeleteListner(task);
+    });
+
+
+    //edit task
 
 });
--- index.php
+++ index.php
@@ -26,7 +26,7 @@ if (!(isset($_SESSION["isauthenticat"]) && $_SESSION["isauthenticat"])) {
     <div class="page d-flex">
         <section class="sidebar sticky-top text-center py-4 px-3">
             <img src="assets/images/logo.png" alt="">
-            <button type="button" name="add task" id="addTaskBtn" class="add-task-btn btn shadow-sm mt-5 mx-auto d-flex align-items-center gap-4 py-2 px-3">
+            <button type="button" data-bs-toggle="modal" data-bs-target="#addTaskModal" name="add task" id="addTaskBtn" class="add-task-btn btn shadow-sm mt-5 mx-auto d-flex align-items-center gap-4 py-2 px-3">
                 <span class="d-block text-capitalize fw-medium">add task</span>
                 <div class="add-task-icon bg-main-red rounded-pill d-flex align-items-center justify-content-center">
                     <i class="fa-solid fa-plus fa-beat text-main-white"></i>
@@ -56,7 +56,7 @@ if (!(isset($_SESSION["isauthenticat"]) && $_SESSION["isauthenticat"])) {
         <section class="main w-100">
             <header class="header sticky-top py-3 px-4 d-flex justify-content-between align-items-center">
                 <h1 class="fs-3 text-capitalize">dashboard</h1>
-                <a href="controllers/sign_out.php" type="button" name="" id="" class="btn text-white main-btn d-flex align-items-center gap-2">
+                <a href="controllers/sign_out.php" type="button" name="" id="" class="btn text-white main-btn-red d-flex align-items-center gap-2">
                     <span class="text-capitalize">Sign out</span>
                     <i class="fa-solid fa-arrow-right-from-bracket"></i>
                 </a>
@@ -77,46 +77,44 @@ if (!(isset($_SESSION["isauthenticat"]) && $_SESSION["isauthenticat"])) {
                         <span id="DeleteAllTasks" role="button" class="text-main-red">Delete all</span>
                     </div>
                     <ul id="TasksParent" class="list-unstyled mt-3">
-                        <li class="shadow-sm bg-main-white rounded mb-3 task">
+                        <li class="shadow-sm bg-main-white rounded mb-3 d-flex align-items-center task">
                             <input id="taskid1" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
                             <label role="button" for="taskid1" class="w-100 p-3">
                                 <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
                                 <span class="">Buy monthly groceries</span>
                             </label>
-                        </li>
-                        <li class="shadow-sm bg-main-white rounded mb-3 task">
-                            <input id="taskid2" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
-                            <label role="button" for="taskid2" class="w-100 p-3">
-                                <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
-                                <span class="">Buy monthly groceries</span>
-                            </label>
-                        </li>
-                        <li class="shadow-sm bg-main-white rounded mb-3 task">
-                            <input id="taskid3" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
-                            <label role="button" for="taskid3" class="w-100 p-3">
-                                <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
-                                <span class="">Buy monthly groceries</span>
-                            </label>
-                        </li>
-                        <li class="shadow-sm bg-main-white rounded mb-3 task">
-                            <input id="taskid4" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
-                            <label role="button" for="taskid4" class="w-100 p-3">
-                                <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
-                                <span class="">Buy monthly groceries</span>
-                            </label>
-                        </li>
-                        <li class="shadow-sm bg-main-white rounded mb-3 task">
-                            <input id="taskid5" type="checkbox" class="task__input d-none" value="Buy monthly groceries">
-                            <label role="button" for="taskid5" class="w-100 p-3">
-                                <i class="task-icon text-main-orange fa-regular fa-square me-2"></i>
-                                <span class="">Buy monthly groceries</span>
-                            </label>
+                            <i role="button" class="fa-solid fa-pen-to-square text-main-orange me-3 edit-task-btn"></i>
+                            <i role="button" class="fa-solid fa-trash text-main-orange me-3 delete-task-btn"></i>
                         </li>
                     </ul>
                 </section>
             </section>
         </section>
     </div>
+
+    <!-- Modals -->
+    <div class="modal fade" data-bs-backdrop="static" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title text-capitalize fs-5 fw-semibold" id="addTaskModal">add task</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="group mb-3">
+                        <label for="taskName" class="form-label">Task Name:</label>
+                        <input type="text" class="form-control form-control-md border border-secondary-subtle" name="task name" id="taskName" aria-describedby="taskNameHelp" placeholder="" />
+                        <small id="taskNameHelp" class="form-text text-body-secondary"></small>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">cancel</button>
+                    <button id="SaveTaskBtn" type="button" class="btn main-btn-red">add task</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script src="assets/scripts/home.js"></script>
     <!-- Bootstrap JavaScript Libraries -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
